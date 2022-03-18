bbd994bbfcd0bc1f7a4b3a7697665edd08094b35
AnnaSemichaeva <tdhnss@gmail.com>
2022-03-18 16:55:56 +0300

    all buttons with numbers are connected to one function

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func digits(_ sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
